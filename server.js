c101f61339d4db9dd3740c78888aeb61728689f2
Alexzen90 <aporteron90@gmail.com>
2024-07-23 10:24:52 +0200

    add authorizations

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ const DatabaseMiddleware = require('./middlewares/database')
 const LoggerMiddleware = require('./middlewares/logger')
 
 app.use(bodyParser.json(), LoggerMiddleware.addLogger)
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 /*--------------------- Création des routes (User - Utilisateur) ---------------------*/
 
