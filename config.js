9f56762025fa87e9a25548392748c9f9d1dce1d5
jha <haimezjohn@gmail.com>
2024-09-09 13:17:35 +0200

    fix docker

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
+require("dotenv").config();
+
 module.exports.port = process.env.PORT || 3001;
 module.exports.secret_key = process.env.SECRET_KEY || "MY_SECRET_KEY_HASH";
 module.exports.secret_cookie = process.env.SECRET_COOKIE || "COOKIE";
-module.exports.url_database = process.env.URL_DATABASE || "mongodb://localhost:27017";
+module.exports.url_database =
+  `${process.env.URL_DATABASE}:${process.env.PORT_BDD}` ||
+  "mongodb://localhost:27017";
--- utils/database.js
+++ utils/database.js
@@ -1,13 +1,21 @@
-const mongoose = require('mongoose');
-const Logger = require('./logger').pino
-const Config = require('../config')
+const mongoose = require("mongoose");
+const Logger = require("./logger").pino;
+const Config = require("../config");
 
-mongoose.connection.on('connected', () => Logger.info('connected'));
-mongoose.connection.on('open', () => Logger.info('open'));
-mongoose.connection.on('disconnected', () => Logger.error('disconnected'));
-mongoose.connection.on('reconnected', () => Logger.info('reconnected'));
-mongoose.connection.on('disconnecting', () => Logger.error('disconnecting'));
-mongoose.connection.on('close', () => Logger.info('close'));
+mongoose.connection.on("connected", () => Logger.info("connected"));
+mongoose.connection.on("open", () => Logger.info("open"));
+mongoose.connection.on("disconnected", () => Logger.error("disconnected"));
+mongoose.connection.on("reconnected", () => Logger.info("reconnected"));
+mongoose.connection.on("disconnecting", () => Logger.error("disconnecting"));
+mongoose.connection.on("close", () => Logger.info("close"));
 
-mongoose.connect(`${Config.url_database}/${process.env.npm_lifecycle_event == 'test' ? "QUIZ_SERVER_TEST" : "QUIZ_SERVER_PRODUCTION"}`, {
-  useUnifiedTopology: true});
+mongoose.connect(
+  `${Config.url_database}/${
+    process.env.npm_lifecycle_event == "test"
+      ? "QUIZ_SERVER_TEST"
+      : "QUIZ_SERVER_PRODUCTION"
+  }`,
+  {
+    useUnifiedTopology: true,
+  }
+);
